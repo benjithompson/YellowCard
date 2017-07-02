77e1a1871db74a17972ec82b211e7885fec2f2b7
benjithompson <mail@benjithompson.com>
2017-07-02 13:23:26 -0500

    init commit with config details

---
+++ main.py
@@ -0,0 +1,9 @@
+"""Starts bot on twitter that tweets when internet bandwidth
+   goes lower than advertised"""
+
+"""TODO:
+    -create new twitter acct
+    -Connect to twitter api using oath
+    -setup cli params for configuring polling rate, bandwidth-limit and company
+    -check bandwidth
+    -post if results meet criteria"""
